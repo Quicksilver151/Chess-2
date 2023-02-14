cabaeb7b42312df523ccc62388fd6e5a4b4ad012
Default Cube <56493793+Quicksilver151@users.noreply.github.com>
2023-02-14 21:45:42 +0500

    added movement types
    - Todo: add move validation

--- Code/Moves.gd
+++ Code/Moves.gd
@@ -1,8 +1,17 @@
 extends TileMap;
 
+enum TYPE{Normal, Over};
+
+var moves: Dictionary = {
+	"pos" : PackedVector2Array(),
+	"type"  : TYPE,
+}
+
+@export var Type: TYPE = TYPE.Normal
 
 func fetch_moves():
 	var tiles = get_used_cells(0);
-	var moves = PackedVector2Array(tiles);
+	moves.pos = PackedVector2Array(tiles);
+	moves.type = Type
 	return moves;
 
--- Code/Piece.gd
+++ Code/Piece.gd
@@ -1,23 +1,19 @@
 extends Node2D;
 class_name Piece;
 
-
 var moves: PackedVector2Array;
 
-
 func _ready():
+	update_moves()
 	for child in get_children():
-		if child is TileMap and child.visible:
-			moves.append_array(child.fetch_moves())
-		
-		child.visibility_changed.connect(on_visibility_changed)
+		child.visibility_changed.connect(update_moves)
 
 
-func on_visibility_changed():
+func update_moves():
 	moves = PackedVector2Array()
 	for child in get_children():
 		if child is TileMap and child.visible:
-			moves.append_array(child.fetch_moves())
+			moves.append_array(child.fetch_moves().pos)
 
 
 func _input(event):
@@ -25,15 +21,14 @@ func _input(event):
 		if event.is_pressed():
 			var relative_mouse_grid_pos = ((get_global_mouse_position() - position)/32).round()
 			print(relative_mouse_grid_pos)
-			if relative_mouse_grid_pos in moves:
+			if relative_mouse_grid_pos in moves and is_move_valid():
 				move(relative_mouse_grid_pos)
 				print("kkk")
 
+func is_move_valid():
+	#todo
+	return true
 
 func move(pos):
 	position += pos*32
 
-
-
-func _on_sprite_visibility_changed():
-	pass # Replace with function body.
